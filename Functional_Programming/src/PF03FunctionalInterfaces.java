fa46063e3bea7000f19ba3371b948ddcfacbefc4
HugoSanche <hbmclaren@gmail.com>
2023-09-02 23:16:38 -0600

    BehaviorParameterization

--- Functional_Programming/src/PF03FunctionalInterfaces.java
+++ Functional_Programming/src/PF03FunctionalInterfaces.java
@@ -1,8 +1,6 @@
 import java.util.List;
-import java.util.function.BinaryOperator;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Predicate;
+import java.util.Random;
+import java.util.function.*;
 
 public class PF03FunctionalInterfaces {
     /*
@@ -17,6 +15,7 @@ public class PF03FunctionalInterfaces {
     public static void main(String[] args) {
         List<Integer> numbers=List.of(12,9,13,4,6,2,4,12,15);
 
+        System.out.println("PREDICATE");
         /*PREDICATE.- Received one argument and return a boolean*/
         Predicate<Integer> isEvenPredicate =  x -> x % 2 == 0;
 
@@ -28,6 +27,9 @@ public class PF03FunctionalInterfaces {
             }
         };
 
+
+        /*---------------------------------------------------------------------------------------------------------*/
+        System.out.println("FUNCTION");
         /*FUNCTION.-Received one argument a return a Integer */
         Function<Integer, Integer> squareFunction = x -> x * x;
 
@@ -39,6 +41,8 @@ public class PF03FunctionalInterfaces {
             }
         };
 
+        /*---------------------------------------------------------------------------------------------------------*/
+        System.out.println("CONSUMER");
         /*
         * CONSUMER.- Received one argument and don't return anything
         * */
@@ -58,6 +62,8 @@ public class PF03FunctionalInterfaces {
                 .map(squareFunction2)
                 .forEach(sysoutConsumer2);
 
+        /*---------------------------------------------------------------------------------------------------------*/
+        System.out.println("BINARY-OPERATOR");
         //Exercise 12;
         /*
         * BINARYOPERATOR.- Received 2 arguments and return one integer
@@ -74,8 +80,39 @@ public class PF03FunctionalInterfaces {
 
         numbers
                 .stream()
-                .reduce(0,
-                        sumBinaryOperator2);
+                .reduce(0, sumBinaryOperator2);
 
+        //System.out.println(numbers);
+
+        /*---------------------------------------------------------------------------------------------------------*/
+        System.out.println("SUPPLIER");
+        /*
+        * SUPPLIER.- No arguments return something
+        * */
+        //No input > Return something
+
+        Supplier<Integer> ramdomIntegerSupplier=()->{
+            Random random=new Random();
+            return random.nextInt(1000);
+        };
+        Supplier<String> string=()->{
+          return "Hugo Baltazar";
+        };
+        System.out.println(ramdomIntegerSupplier.get());
+        System.out.println(string.get());
+
+        /*---------------------------------------------------------------------------------------------------------*/
+        System.out.println("UNARY OPERATOR");
+        /*
+         * UNARY OPERATOR.- 1 argument return something
+         * */
+        UnaryOperator<Integer> unaryOperator=x->3+x;
+        System.out.println(unaryOperator.apply(35));
+
+        /*
+        * Difference between Function & UnaryOperator
+        *
+        * Function can have a different type of input and output. UnaryOperator should have the same type.
+        * */
     }
 }
